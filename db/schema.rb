4326f2175f023cdd1121829ae5d0cde0a15ec83d
rofiuddin <md.rofi.uddin@framgia.com>
2015-12-01 12:28:17 +0600

    add devise for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130071733) do
+ActiveRecord::Schema.define(version: 20151201040850) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id",       limit: 4
@@ -68,15 +68,25 @@ ActiveRecord::Schema.define(version: 20151130071733) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "firstname",       limit: 255
-    t.string   "lastname",        limit: 255
-    t.string   "email",           limit: 255
-    t.text     "password_digest", limit: 65535
-    t.text     "remember_digest", limit: 65535
-    t.boolean  "supervisor"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.boolean  "supervisor",                         default: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "users_courses", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+## supervisors
+ User.create!(first_name: "test",
+              last_name: "test",
+              email: "test@test.com",
+              password: "password",
+              supervisor: true)
+ User.create!(first_name: "test1",
+              last_name: "test1",
+              email: "test1@test.com",
+              password: "password",
+              supervisor: true)
+## trainees
+99.times do |n|
+  first_name = Faker::Name.first_name
+  last_name =  Faker::Name.last_name
+  email = "example-#{n+1}@test.com"
+  password = "password"
+  User.create!(first_name:  first_name,
+               last_name: last_name,
+               email: email,
+               password: password)
+end
