ec38d1d40fc1ec8f53e168061b1f392fc3d55750
rofiuddin <md.rofi.uddin@framgia.com>
2015-12-01 18:16:40 +0600

    customise devise views for authentication

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,7 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    render 'app/views/layouts/devise'
+  end
+end
