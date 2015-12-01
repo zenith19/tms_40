aaf8fffb698fd1a0de0362a32880a73c5a0bf2c0
rofiuddin <md.rofi.uddin@framgia.com>
2015-12-01 18:16:40 +0600

    show user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_user, only: [:show]
+
   def index
     @users = User.trainees.paginate page: params[:page], per_page: 15
+  end
+  def show
+    @activities = @user.activities.paginate page: params[:page]
+  end
+
+  private
+  def load_user
+    @user = User.find params[:id]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,19 @@ class User < ActiveRecord::Base
     trainee: '0',
     supervisor: '1'
   }
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :activities
+  has_many :users_course
   has_many :courses, through: :users_course
+  has_many :users_subjects
   has_many :subjects, through: :users_subject
   has_many :tasks, through: :users_tasks
 
   scope :supervisors, -> { where(supervisor: true) }
   scope :trainees, -> { where(supervisor: false) }
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  def full_name
+    first_name+ " " + last_name
+  end
 end
