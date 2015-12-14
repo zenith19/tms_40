2212d021d9f8a1a8e123c070419f85d5aca846f5
zenithit <zenith.it.ju@gmail.com>
2015-12-14 15:24:44 +0600

    48358_add_progress_bars_in_trainee_home

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  has_many :courses_subjects_tasks
+  has_many :courses_subjects, through: :courses_subjects_tasks
   has_many :users_tasks
   has_many :users, through: :users_tasks
 end
