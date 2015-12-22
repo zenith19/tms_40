0f129cd253fcf48f4b8fbc7c3c25e511b84b7c72
Rownaknira <rownaknira@yahoo.com>
2015-12-22 17:00:53 +0600

    Rspec for Subject Model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,9 +3,11 @@ class Subject < ActiveRecord::Base
   tracked owner: ->(controller, model) {controller && controller.current_user}
   acts_as_paranoid
 
+  validates :name, presence: true
+  validates :description, presence: true
   has_many :tasks , dependent: :destroy
   has_many :users_subject
   has_many :users, through: :users_subject
-  has_many :course_subjects, dependent: :destroy
+  has_many :courses_subjects, dependent: :destroy
   accepts_nested_attributes_for :tasks, allow_destroy: true
 end
---
+++ spec/models/subject_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe Subject do
+
+  it "has a valid factory" do
+    expect(FactoryGirl.create :subject).to be_valid
+  end
+
+  it {expect validate_presence_of :name}
+  it {expect validate_presence_of :description}
+
+  it {expect have_many :users}
+  it {expect have_many :tasks}
+  it {expect have_many :courses_subjects}
+end
