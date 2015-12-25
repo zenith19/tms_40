5b538bd7dd2fb76fd15812584fb87061ffe3944a
Abdullah All Mehedi <whitepaper321@gmail.com>
2015-12-26 01:18:32 +0600

    RSpec for Supervisor Course View for index action 49602

--- app/controllers/supervisor/courses_controller.rb
+++ app/controllers/supervisor/courses_controller.rb
@@ -2,7 +2,7 @@ class Supervisor::CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :load_course, except: [:index, :new, :create]
   before_action :check_course, only: [:update, :destroy]
-  load_and_authorize_resource except: [:new, :create]
+  load_and_authorize_resource except: [:index, :new, :create]
 
   def index
     @search = current_user.courses.search params[:q]
